ec6588f53a3a9ebf69ef4862da803e51d86caed3
TechNajru <technajru@gmail.com>
2025-09-05 00:51:04 +0530

    Comments

---
+++ Part-2/comments.c
@@ -0,0 +1,19 @@
+// Single line comments
+
+/*Multi
+line
+comments*/
+
+#include <stdio.h>
+int main()
+{
+    // this will print hii and person name
+    printf("hii\n");
+    printf("najru \n \n");
+
+    /*The first will print welcome
+    and secound will print name of user*/
+    printf("Welcome \n");
+    printf("najrudeen ji");
+    return 0;
+}
