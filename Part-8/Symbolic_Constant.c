af6b7135e30883f8dbcb42903764765f5ab3ed09
TechNajru <technajru@gmail.com>
2025-09-05 00:55:37 +0530

    Symbolic_Constant

---
+++ Part-8/Symbolic_Constant.c
@@ -0,0 +1,9 @@
+// Symbolic Constant Compile hone se pahle  value  replace kar deta hai
+
+#include <stdio.h>
+#define print printf
+void main()
+{
+    print("hello \n");
+    print("welcome \nback \nguys");
+}
