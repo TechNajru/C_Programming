2b2698e2305db5e337cb6d01e10d162a4bdcbbcc
TechNajru <technajru@gmail.com>
2025-09-05 00:52:22 +0530

    Indentation

---
+++ Part-3/Indentattion.c
@@ -0,0 +1,11 @@
+/* C is a free form language
+means if we follow rule ,
+lines does not matter */
+
+#include <stdio.h>
+void main(){printf("hello \n");printf("Sir \n \n");
+
+    // Indentation Rule
+    printf("how are you \n");
+    printf("tell me your nickname");
+}
