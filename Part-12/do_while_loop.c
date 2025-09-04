5e941177373e90f1e7d404a46e689e808da77471
TechNajru <technajru@gmail.com>
2025-09-05 01:08:27 +0530

    Loops

---
+++ Part-12/do_while_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1;
+    do
+    {
+        printf("%d \n", x * 2);
+        x++;
+    } while (x <= 10);
+    return 0;
+}
---
+++ Part-12/while_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1;
+    while (x <= 10)
+    {
+        printf("Njarudeen \n");
+        printf("%d \n", x * 2);
+        x++;
+    }
+    return 0;
+}
